bd118655d0a79d20a3903b24f669221f079240f1
reynoldcn <308388682@qq.com>
2016-02-23 20:33:37 -0500

    make it better

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -25,18 +25,7 @@ public class Main {
     port(Integer.valueOf(System.getenv("PORT")));
     staticFileLocation("/public");
 
-    get("/hello", (req, res) -> {
-      RelativisticModel.select();
-      Amount<Mass> m = Amount.valueOf("12 GeV").to(KILOGRAM);
-      return "E=mc^2: 12 GeV = " + m.toString();
-    });
 
-    get("/", (request, response) -> {
-            Map<String, Object> attributes = new HashMap<>();
-            attributes.put("message", "Hello World!");
-
-            return new ModelAndView(attributes, "index.ftl");
-        }, new FreeMarkerEngine());
 
     get("/db", (req, res) -> {
       Connection connection = null;
